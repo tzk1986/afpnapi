ccf6858965d7cc887207dd65648c2a5528b3a83b
tzk1986 <tzk1986@gmail.com>
2024-11-15 19:07:57 +0800

    更新run文件目录，执行多页面

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import seldom
 
+# 测试环境地址
+test_url ="http://10.50.11.120:9001"
+
+
 if __name__ == '__main__':
     seldom.main(
-        path="./test_dir",  # 运行用例目录
-        base_url="https://httpbin.org",  # 基础URL地址
-        rerun=3   # 重跑次数
+        path="./test_dir/test_afpn",  # 运行用例目录
+        base_url=test_url,  # 基础URL地址
+        tester="tzk",  # 测试人员
+        # debug=True,  # 是否调试模式
+        
+        # rerun=3   # 重跑次数
     )
---
+++ test_dir/test_afpn/test_foodsafety.py
@@ -0,0 +1,75 @@
+import seldom
+
+
+class TestRequest(seldom.TestCase):
+    """
+    前厅运营页面
+    """
+
+    def start(self):
+        print("开始测试")
+        self.s = self.Session()
+        self.s.post(
+            "/api/uims/login",
+            json={
+                "systemName": "ifood-operating-manage",
+                "authenType": 1,
+                "password": "123456",
+                "phoneNumber": "18930410921",
+                "smsCode": "",
+                "kaptchaCode": "1",
+                "kaptchaKey": "65eb74b59e224db1b8f6ba3266848380",
+            },
+        )
+        self.assertStatusCode(200)
+        print("登录成功")
+
+    def end(self):
+        print("结束测试")
+
+    def test_foodsafety_health(self):
+        """
+        健康证
+        """
+        tt = self.jsonpath("$..token", index=0)
+        print(tt)
+        self.s.post(
+            "/api/mer/healthCert/queryPage",
+            json={
+                "pageNum": 1,
+                "pageSize": 10,
+                "merchantId": "2021040701",
+                "storeId": None,
+                "healthExpireStartTime": None,
+                "healthExpireEndTime": None,
+            },
+            headers={"token": tt},
+        )
+        self.assertStatusCode(200)
+        self.assertPath("errCode", 0)
+
+    def test_foodsafety_post(self):
+        """
+        岗位管理
+        """
+        tt = self.jsonpath("$..token", index=0)
+        print(tt)
+        self.s.post(
+            "/api/mer/healthPost/queryPage",
+            json={
+                "pageNum": 1,
+                "postId": "",
+                "pageSize": 10,
+                "merchantId": "2021040701",
+            },
+            headers={"token": tt},
+        )
+        self.assertStatusCode(200)
+        self.assertPath("errCode", 0)
+
+
+if __name__ == "__main__":
+    seldom.main(
+        # debug=True,
+        base_url="http://10.50.11.120:9001"
+    )
